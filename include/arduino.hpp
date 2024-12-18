67d90a82a984676d8d0dfa6010d54f1fb447661d
Ludovic Bouchard <ludovicb1239@gmail.com>
2024-12-18 16:27:53 +0100

    reverted changes

--- include/arduino.hpp
+++ include/arduino.hpp
@@ -9,7 +9,7 @@ class Arduino : public I2CDevice {
     using I2CDevice::I2CDevice;
    
    public:
-    Arduino();
+    Arduino(int slave_address);
     int servoPosition(int servoNb, int position);
     int readCapteur(int capteurNumber, int &state);
     int moveStepper(int absPosition, int stepperNb);
