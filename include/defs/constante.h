23347578765a4635962f5746d22586adabad6073
Ludovic <ludo@ludo.ordi>
2025-02-10 16:00:54 +0100

    stock pos

--- include/defs/constante.h
+++ include/defs/constante.h
@@ -29,7 +29,36 @@
 #define SPEED_STOCK 8000 // Motor speed for collecting stock (mm/s)
 #define STEPPER_SPEED 1000 // Stepper motor speed for collecting stock (steps/s)
 
+#define COL_OFFSET_STOCK 300 //find collinear offset to take a stock 
+#define NO_COL_OFFSET_STOCK 200 //find non ollinear offset to take a stock 
+
 const int rayon[3] = {200,200,250};
-const position_t STOCK_POSITION_ARRAY[] = {{100, 100, 0}, {200, 200, 90}}; // TODO : Fill this in correctly
+const position_t STOCK_POSITION_ARRAY[] = {{2175, 1725, 0}, {2925, 1325, 90}, {2925, 400, 90}, {2225, 250, 0}, {1900, 950, 0}, 
+                                            {1100, 950, 0}, {775, 250, 0}, {75, 400, 90}, {75, 1325, 90}, {825, 1725, 0}};
+
+const int STOCK_OFFSETS[][2] = {
+    {COL_OFFSET_STOCK, -NO_COL_OFFSET_STOCK},  //0 : Offset for horizontal droite bas
+    {COL_OFFSET_STOCK, NO_COL_OFFSET_STOCK},  //1 : Offset for horizontal droite haut
+    {-COL_OFFSET_STOCK, -NO_COL_OFFSET_STOCK},  //2 : Offset for horizontal gauche bas
+    {-COL_OFFSET_STOCK, NO_COL_OFFSET_STOCK},  //3 : Offset for horizontal gauche haut
+
+    {NO_COL_OFFSET_STOCK, -COL_OFFSET_STOCK},  //4 : Offset for vertical droite bas
+    {NO_COL_OFFSET_STOCK, COL_OFFSET_STOCK},  //5 : Offset for vertical droite haut
+    {-NO_COL_OFFSET_STOCK, -COL_OFFSET_STOCK},  //6 : Offset for vertical gauche bas
+    {-NO_COL_OFFSET_STOCK, COL_OFFSET_STOCK},  //7 : Offset for vertical gauche haut
+}; 
+const int STOCK_OFFSET_MAPPING[10][4] = {
+    {0, -1, -1,-1},   // Stock 0 utilise les offsets 0
+    {6, 7, -1,-1},   // Stock 1 utilise les offsets 6 et 7
+    {6, 7, -1,-1},   // Stock 2 utilise les offsets 6 et 7
+    {1, 3, -1,-1},   // Stock 3 utilise les offsets 1 et 3
+    {0, 1, 2, 3},   // Stock 4 utilise les offsets 0,1,2 et 3
+    {0, 1, 2, 3},   // Stock 5 utilise les offsets 0,1,2 et 4
+    {1, 3, -1,-1},   // Stock 6 utilise les offsets 1 et 3
+    {4, 5, -1,-1},   // Stock 7 utilise les offsets 4 et 5
+    {4, 5, -1,-1},   // Stock 8 utilise les offsets 4 et 5
+    {2, -1, -1,-1}    // Stock 9 utilise les offsets 
+};
+
 // Angle is either 0 for horizontal stock of 90 for vertical
 const int STOCK_COUNT = 2; // and this
