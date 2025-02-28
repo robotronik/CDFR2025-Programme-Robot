71b44d558fba53bb0ea18aead3289a2d8a89c624
Ludovic Bouchard <ludovicb1239@gmail.com>
2025-02-28 14:53:01 +0100

    smolfix

--- include/vision/ArucoCam.hpp
+++ include/vision/ArucoCam.hpp
@@ -8,7 +8,7 @@ private:
     int id;
     bool status; // true if the camera is running, false otherwise
 public:
-    ArucoCam(int cam_number, char* calibration_file_path);
+    ArucoCam(int cam_number, const char* calibration_file_path);
     ~ArucoCam();
     bool getPos(int & x, int & y, int & theta);
 private:
