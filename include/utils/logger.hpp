a276cba94711c1302e3a6ff7f980299e8a802b54
Dalle <guillaume.dalle@grenoble-inp.org>
2025-04-10 22:40:07 +0200

    log in file asserv ( + cache sync)

--- include/utils/logger.hpp
+++ include/utils/logger.hpp
@@ -7,6 +7,10 @@
 #include <ctime>
 #include <iomanip>
 #include <exception>
+#include <fcntl.h>
+#include <unistd.h>
+#include <map>
+#include <sys/stat.h>
 #include "utils/utils.h"
 
 enum class LogLevel {
@@ -22,6 +26,83 @@ enum class LogLevel {
 constexpr LogLevel CURRENT_LOG_LEVEL = LogLevel::DEBUG;
 
 namespace SimpleLogger {
+
+inline static std::map<std::string, int> logFiles;
+inline static bool initOnceFlag = false;
+inline static int globalLogNum = -1;
+
+inline void ensureLogDirectoryAndNumFile(const std::string& logDir = "log", const std::string& numLogFile = "log/numLog") {
+    struct stat st;
+    if (stat(logDir.c_str(), &st) != 0) {
+        if (mkdir(logDir.c_str(), 0755) != 0) {
+            perror("Failed to create log directory");
+            exit(EXIT_FAILURE);
+        }
+        std::ofstream out(numLogFile);
+        if (!out) {
+            std::cerr << "Erreur : impossible de créer le fichier " << numLogFile << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        out << 0;
+        out.close();
+    } else {
+        std::ifstream in(numLogFile);
+        if (!in) {
+            std::cerr << "Erreur : le dossier '" << logDir << "' existe mais pas le fichier 'numLog'. Abandon." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+inline int readAndIncrementLogNum(const std::string& numFilePath = "log/numLog") {
+    ensureLogDirectoryAndNumFile("log",numFilePath);
+    std::ifstream in(numFilePath);
+    int num = 0;
+    if (in >> num) {
+        in.close();
+    }
+
+    std::ofstream out(numFilePath, std::ios::trunc);
+    out << (num + 1);
+    return num;
+}
+
+inline int getLogFileDescriptor(const std::string& baseName) {
+    auto it = logFiles.find(baseName);
+    if (it != logFiles.end()) {
+        return it->second;
+    }
+
+    if(initOnceFlag == false){
+        globalLogNum = readAndIncrementLogNum();
+        initOnceFlag = true;
+    }
+
+    std::ostringstream oss;
+    oss << "log/" << baseName << "_" << globalLogNum << ".log";
+    std::string fullName = oss.str();
+
+    int fd = open(fullName.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1) {
+        perror(("Failed to open log file: " + fullName).c_str());
+        return -1;
+    }
+
+    logFiles[baseName] = fd;
+    return fd;
+}
+
+inline void logToFile(const std::string& baseName, const std::string& message, bool forceSync) {
+    int fd = getLogFileDescriptor(baseName);
+    if (fd == -1) return;
+
+    std::string fullMessage = message + "\n";
+    write(fd, fullMessage.c_str(), fullMessage.size());
+
+    if (forceSync) {
+        fsync(fd);
+    }
+}
 
 inline std::string getLevelString(LogLevel level) {
     switch (level) {
@@ -60,31 +141,42 @@ static std::string previousMessage;
 static unsigned long previousMessageTime;
 
 template<typename... Args>
-inline void log(LogLevel level, const std::string& functionName, const int line, const std::string& message, const Args&... args) {
-    if (level < CURRENT_LOG_LEVEL)
+inline void log(LogLevel level, const std::string& functionName, const int line, const std::string& file, bool force, const std::string& message, const Args&... args) {
+
+    bool stdOutNotValid = (level < CURRENT_LOG_LEVEL) || (message == previousMessage && _millis() / 1000 == previousMessageTime);
+
+    if(stdOutNotValid && file.empty())
         return;
-    if (message == previousMessage && _millis() / 1000 == previousMessageTime)
-        return;
+
     std::ostringstream oss;
     appendMessage(oss, message, args...);
 
-    std::string colorCode = getColorCode(level);
-    std::string resetCode = "\033[0m";
-
-    std::ostringstream logStream;
-    logStream << colorCode
-            << currentTimeFormatted() << " "
+    std::ostringstream playLoad;
+            playLoad << currentTimeFormatted() << " "
             << std::left << std::setw(10) << ("[" + getLevelString(level) + "]") // Fixed width for log level
             << std::left << std::setw(30) << ("[" + functionName + ":" + std::to_string(line) + "]") << "  " // Fixed width for function name
-            << oss.str()
-            << resetCode << std::endl;
+            << oss.str();
 
-    // Print the fully constructed log message in one operation.
-    std::cout << logStream.str();
-    std::cout.flush();  // Explicit flush to ensure immediate writing
+    if(!stdOutNotValid){
+        std::string colorCode = getColorCode(level);
+        std::string resetCode = "\033[0m";
 
-    previousMessageTime = _millis() / 1000;
-    previousMessage = message;
+        std::ostringstream logStream;
+        logStream << colorCode
+                << playLoad.str()
+                << resetCode << std::endl;
+
+        // Print the fully constructed log message in one operation.
+        std::cout << logStream.str();
+        std::cout.flush();  // Explicit flush to ensure immediate writing
+
+        previousMessageTime = _millis() / 1000;
+        previousMessage = message;
+    }
+
+    if (!file.empty()) {
+        logToFile(file, playLoad.str(), force);
+    }
 }
 
 } // namespace SimpleLogger
@@ -97,20 +189,22 @@ inline void initLog(void){
     std::cout << " |  _  /| |  | |  _ <| |  | | | |  |  _  /| |  | | .   | | | |  <  " << std::endl;
     std::cout << " | | \\ \\| |__| | |_) | |__| | | |  | | \\ \\| |__| | |\\  |_| |_| . \\ " << std::endl;
     std::cout << " |_|  \\_\\\\____/|____/ \\____/  |_|  |_|  \\_\\\\____/|_| \\_|_____|_|\\_\\" << std::endl;
-    std::cout << "\n\033[0m";                                                           
-                                                                
+    std::cout << "\n\033[0m";
+
     std::cout << "ROBOTRONIK" << std::endl;
     std::cout << "PROGRAM ROBOT CDFR" << std::endl;
     std::cout << "Start Time : " << currentTimeFormatted() << std::endl;
 }
 
+
 // Convenience macros that automatically pass the calling function's name.
-#define LOG_DEBUG(message, ...)      SimpleLogger::log(LogLevel::DEBUG,    __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define LOG_INFO(message, ...)       SimpleLogger::log(LogLevel::INFO,     __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define LOG_WARNING(message, ...)    SimpleLogger::log(LogLevel::WARNING,  __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define LOG_ERROR(message, ...)      SimpleLogger::log(LogLevel::ERROR,    __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define LOG_GREEN_INFO(message, ...) SimpleLogger::log(LogLevel::GREENINFO,__FILE__, __LINE__, message, ##__VA_ARGS__)
-#define LOG_ASSERV_SET_INFO(message, ...) SimpleLogger::log(LogLevel::I2C, __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define LOG_ASSERV_GET_INFO(message, ...) SimpleLogger::log(LogLevel::I2C, __FILE__, __LINE__, message, ##__VA_ARGS__)
+#define LOG_DEBUG(message, ...)      SimpleLogger::log(LogLevel::DEBUG,    __FILE__, __LINE__, "", false, message, ##__VA_ARGS__)
+#define LOG_INFO(message, ...)       SimpleLogger::log(LogLevel::INFO,     __FILE__, __LINE__, "", false, message, ##__VA_ARGS__)
+#define LOG_WARNING(message, ...)    SimpleLogger::log(LogLevel::WARNING,  __FILE__, __LINE__, "", false, message, ##__VA_ARGS__)
+#define LOG_ERROR(message, ...)      SimpleLogger::log(LogLevel::ERROR,    __FILE__, __LINE__, "", false, message, ##__VA_ARGS__)
+#define LOG_GREEN_INFO(message, ...) SimpleLogger::log(LogLevel::GREENINFO,__FILE__, __LINE__, "", false, message, ##__VA_ARGS__)
+
+#define LOG_ASSERV_SET_INFO(message, ...) SimpleLogger::log(LogLevel::I2C, __FILE__, __LINE__, "Asserv", true, message, ##__VA_ARGS__)
+#define LOG_ASSERV_GET_INFO(message, ...) SimpleLogger::log(LogLevel::I2C, __FILE__, __LINE__, "Asserv", false, message, ##__VA_ARGS__)
 
 #define LOG_INIT() initLog();
