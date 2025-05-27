771da0399e41d67d196f4624494e823c22307601
Ludovic Bouchard <ludovicb1239@gmail.com>
2025-05-27 12:13:00 +0200

    bugfix

--- src/navigation/navigation.cpp
+++ src/navigation/navigation.cpp
@@ -38,7 +38,7 @@ nav_return_t navigationGoTo(int x, int y, int theta, Direction direction, Rotati
                 return NAV_ERROR;
             }else{
                 LOG_DEBUG("Going to point with highways : ", x, " ", y);
-                if (!asserv.get_command_buffer_size() != 0)
+                if (asserv.get_command_buffer_size() != 0)
                     asserv.stop();
                 for (int i = 0; i < currentPathLenght; i++){
                     asserv.go_to_point(currentPath[i].x,currentPath[i].y, i == 0 ? rotationLookAt : Rotation::SHORTEST, direction);
@@ -48,7 +48,7 @@ nav_return_t navigationGoTo(int x, int y, int theta, Direction direction, Rotati
             }
         }else{
             LOG_DEBUG("Going to point without highways : ", x, " ", y);
-            if (!asserv.get_command_buffer_size() != 0)
+            if (asserv.get_command_buffer_size() != 0)
                 asserv.stop();
             asserv.go_to_point(x,y, theta, rotationLookAt, direction, rotation);
             currentPath[0] = {x,y};
@@ -87,7 +87,7 @@ nav_return_t navigationGoToNoTurn(int x, int y, Direction direction, Rotation ro
                 return NAV_ERROR;
             }else{
                 LOG_DEBUG("Going to point with highways : ", x, " ", y);
-                if (!asserv.get_command_buffer_size() != 0)
+                if (asserv.get_command_buffer_size() != 0)
                     asserv.stop();
                 for (int i = 0; i < currentPathLenght; i++){
                     asserv.go_to_point(currentPath[i].x,currentPath[i].y, i == 0 ? rotationLookAt : Rotation::SHORTEST, direction);
@@ -97,7 +97,7 @@ nav_return_t navigationGoToNoTurn(int x, int y, Direction direction, Rotation ro
         }
         else{
             LOG_DEBUG("Going to point without highways : ", x, " ", y);
-            if (!asserv.get_command_buffer_size() != 0)
+            if (asserv.get_command_buffer_size() != 0)
                 asserv.stop();
             //asserv.set_brake_state(false);
             asserv.go_to_point(x,y, rotationLookAt, direction);
